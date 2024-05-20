b1147f21f418f29b51116c1350423d3d8cf705d0
danielApopei <118684606+danielApopei@users.noreply.github.com>
2024-05-20 18:50:11 +0300

    feat: lab11 compulsory

---
+++ lab11/src/main/java/com/example/lab11/author/Author.java
@@ -0,0 +1,48 @@
+package com.example.lab11.author;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    public Author(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Author() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ lab11/src/main/java/com/example/lab11/author/AuthorController.java
@@ -0,0 +1,24 @@
+package com.example.lab11.author;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path="api/authors")
+public class AuthorController {
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public List<Author> getAuthors() {
+        return authorService.getAllAuthors();
+    }
+}
